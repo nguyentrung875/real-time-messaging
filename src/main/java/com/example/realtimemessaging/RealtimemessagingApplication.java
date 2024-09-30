365ce99cd6758007c1986afc5fadfd66dce20e98
Trung Nguyen <nguyentrung875@gmail.com>
2024-09-30 13:51:13 +0700

    websocket and kafka

---
+++ src/main/java/com/example/realtimemessaging/RealtimemessagingApplication.java
@@ -0,0 +1,13 @@
+package com.example.realtimemessaging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealtimemessagingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealtimemessagingApplication.class, args);
+	}
+
+}
